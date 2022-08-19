550bacad80dd57956bf07c5f2dcdfd406a5064b4
Alex <dm.alexandru92@gmail.com>
2022-08-19 15:00:11 +0300

    swagger jwt bear

--- ReFeelAPI/Program.cs
+++ ReFeelAPI/Program.cs
@@ -1,9 +1,14 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using ReFeelCourtesy.Logging;
 using ReFeelRepository.iRepository;
 using ReFeelRepository.Models;
 using ReFeelRepository.Repository;
+using System.Text;
+
 //using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,15 +23,62 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<RefeelContext>(option => { option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); });
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddAutoMapper(typeof(MappingConfig));
+
+var key = builder.Configuration.GetValue<string>("ApiSettings:SecretToken");
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+    };
+});
 builder.Services.AddControllers( option =>
 {
     option.ReturnHttpNotAcceptable=true;    
 }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{ 
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description =
+            "JWT Authorizationb header using the Bearer scheme . \r\n\r\n " +
+            "Enter 'Bear'[space] and then your token in the text input below. \r\n\r\n " +
+            "Example : \"Bear 12345abccdef\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+        {
+            {
+                new OpenApiSecurityScheme()
+                {
+                    Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                },
+                    Scheme = "oauth2",
+                    Name = "Bearer",
+                    In= ParameterLocation.Header,
+                },
+                new List<string>()
+        }
+    });
+});
 builder.Services.AddSingleton<iLoggin, Logging>();
-
 
 var app = builder.Build();
 
@@ -38,7 +90,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
